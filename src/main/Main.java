bca56333b227e0ba985675e5bf5cf55ec57987a7
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-01-19 21:34:12 +0800

    added import statements

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
